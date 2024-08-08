29f4f8756438871e6754728dff9edee26552e547
GloriaBasisia <gloriabasisia@gmail.com>
2024-08-08 23:06:44 +0200

    Add program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int c = (a + b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
